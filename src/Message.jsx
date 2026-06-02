6ad1d3899da09b91f72cb892998c41c228ba7283
Stephen Oduor <steveotieno2012005@gmail.com>
2026-06-02 20:46:35 +0300

    updated productCard.jsx

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,7 +3,7 @@ function Message() {
 
     const name = 'Jay';
     if (name)
-        return <h1 style={{ color:'tomato',fontSize:'2rem', fontStyle:'italic'}}>Welcome back, {name}</h1>
+        return <h1 style={{ color:'tomato',fontSize:'2rem', fontStyle:'italic', alignItems: "center"}}>Welcome back, {name}</h1>
     return <h1>Welcome New User, Update your username</h1>
 
 
